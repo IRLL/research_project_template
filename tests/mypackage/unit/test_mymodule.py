0fe01ae9f13b388b172ff848ae1543daaff997d1
Mathïs Fédérico <mathfederico@gmail.com>
2022-09-12 06:29:29 +0200

    :sparkles: Fix issue

--- tests/mypackage/unit/test_mymodule.py
+++ tests/mypackage/unit/test_mymodule.py
@@ -5,7 +5,6 @@ from mypackage.mymodule import my_perfect_function
 
 def test_fizz():
     """should return Fizz when divisible by 3 and not 5."""
-    assert False
     check.equal(my_perfect_function(3), "Fizz")
 
 
